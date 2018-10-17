a180ad5917ed1ee5ed64925a0bc829fd03065849
mewturn <pathonice@hotmail.com>
2018-10-17 15:03:35 +0800

    Amendments to API formatting

--- flaskapp.py
+++ flaskapp.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import queries
 import calculate
+import datetime
 
 app = Flask(__name__)
 
@@ -34,15 +35,22 @@ def predict():
     try:
         city = request.args.get("city").lower()
         district = request.args.get("district").lower()
+        weather_data = queries.getLatestWeather(city, district)
+        population_data = queries.getPopulationData(city, district)
 
-        h, t, p, w, r = queries.getLatestWeather(city, district)
-
+        h, t, p, w, r = weather_data["humidity"], weather_data["temperature"], weather_data[
+            "pressure"], weather_data["wind_speed"], weather_data["rain"]
         prob = 0.2
 
-        return jsonify(probability=prob)
+        return jsonify(time=datetime.datetime.now(), city=city, district=district, population=population_data["population"], males=population_data["males"], probability=prob)
 
     except Exception as e:
         print(e)
+
+
+@app.route("/test", methods=["GET"])
+def test():
+    return jsonify(queries.test())
 
 
 if __name__ == "__main__":
