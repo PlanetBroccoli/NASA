ac0ed16d085c83013f219b81a5c76e11fae0f437
mewturn <pathonice@hotmail.com>
2018-10-19 02:38:00 +0800

    Add more queries

--- queries.py
+++ queries.py
@@ -13,6 +13,10 @@ mycursor = mydb.cursor()
 
 def test():
     mycursor.execute("SELECT * FROM lol")
+
+    data_labels = ["humidity", "temperature",
+                   "pressure", "wind_speed", "rain"]
+    data = mycursor.fetchall()
     return mycursor.fetchall()
 
 
@@ -59,10 +63,66 @@ def getPopulationData(city, district, language="en"):
         print(e)
 
 
+def getStation(city, district, language="en"):
+    try:
+        district_languages = {"en": "district_en", "zh": "district"}
+        city_languages = {"en": "city_en", "zh": "city"}
+
+        district_lang, city_lang = district_languages[language], city_languages[language]
+
+        database = "districts"
+
+        condition = "`%s` LIKE '%s' AND `%s` LIKE '%s'" % (
+            city_lang, city, district_lang, district)
+
+        mycursor.execute(
+            "SELECT `station` FROM %s WHERE %s" % (database, condition))
+
+        # Station
+        data_labels = ["station"]
+        data = mycursor.fetchone()
+
+        return generateDictionary(data, data_labels)
+
+    except Exception as e:
+        print(e)
+
+
+def getWeeklyData(year, week, station):
+    try:
+        year_column = "year"
+        week_column = "week"
+        station_column = "station_name"
+        database = "weather"
+
+        condition = "`%s` LIKE '%s' AND `%s` LIKE '%s' AND `%s` LIKE '%s'" % (
+            year_column, year, week_column, week, station_column, station)
+
+        mycursor.execute("SELECT * FROM %s WHERE %s" % (database, condition))
+
+        data_labels = ["id", "year", "month", "week", "day_of_year",
+                       "station_name", "pressure", "max_pressure", "min_pressure", "temperature", "max_temperature", "min_temperature", "humidity", "min_humidity", "wind_speed", "max_gust", "precipitation", "sunshine", "max_uv", "cloud"]
+        data = mycursor.fetchall()
+        for i in data:
+            print(i)
+        return generateDictionary(data, data_labels)
+    except Exception as e:
+        print(e)
+
+
 def generateDictionary(data, labels):
-    output = {}
+    if len(data) == 1:
+        output = {}
+        for i in range(len(labels)):
+            output[labels[i]] = data[i]
+        return output
 
-    for i in range(len(labels)):
-        output[labels[i]] = data[i]
+    else:
+        output_list = []
+        for elem in data:
+            output = {}
+            for i in range(len(labels)):
+                output[labels[i]] = elem[i]
 
-    return output
+            output_list.append(output)
+        return output_list
