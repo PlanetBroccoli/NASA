be2c0b72031a1bd043966f509a4b7329d09b077d
mewturn <pathonice@hotmail.com>
2018-10-19 02:37:47 +0800

    Add calculate function for weekly data

--- calculate.py
+++ calculate.py
@@ -1,4 +1,5 @@
 import numpy as np
+import statistics
 
 
 def calculateProbability(h, t, p, w, r, pop, a):
@@ -18,3 +19,23 @@ def calculateProbability(h, t, p, w, r, pop, a):
     prediction_matrix = np.zeros(len(parameters))
 
     return np.dot(parameters, prediction_matrix)
+
+
+def calculateWeeklyData(ld):
+    p = statistics.mean([d["pressure"] for d in ld if d["pressure"] > 0])
+    max_p = max([d["max_pressure"] for d in ld])
+    min_p = min([d["min_pressure"] for d in ld if d["min_pressure"] > 0])
+    t = statistics.mean([d["temperature"] for d in ld if d["temperature"] > 0])
+    max_t = max([d["max_temperature"] for d in ld])
+    min_t = min([d["min_temperature"]
+                 for d in ld if d["min_temperature"] > 0])
+    h = statistics.mean([d["humidity"] for d in ld if d["humidity"] > 0])
+    min_h = min([d["min_humidity"]
+                 for d in ld if d["min_humidity"] > 0])
+    w = statistics.mean([d["wind_speed"] for d in ld])
+    g = max([d["max_gust"] for d in ld])
+    r = statistics.mean([d["precipitation"] for d in ld])
+    s = statistics.mean([d["sunshine"] for d in ld])
+    max_uv = max([d["max_uv"] for d in ld])
+    c = statistics.mean([d["cloud"] for d in ld])
+    return p, max_p, min_p, t, max_t, min_t, h, min_h, w, g, r, s, max_uv, c
