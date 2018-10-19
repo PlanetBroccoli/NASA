0505d625bd467c225479cdb133b6941ca225d405
mewturn <pathonice@hotmail.com>
2018-10-19 11:46:01 +0800

    Add weekly weather calculations

--- calculate.py
+++ calculate.py
@@ -22,20 +22,25 @@ def calculateProbability(h, t, p, w, r, pop, a):
 
 
 def calculateWeeklyData(ld):
-    p = statistics.mean([d["pressure"] for d in ld if d["pressure"] > 0])
-    max_p = max([d["max_pressure"] for d in ld])
-    min_p = min([d["min_pressure"] for d in ld if d["min_pressure"] > 0])
-    t = statistics.mean([d["temperature"] for d in ld if d["temperature"] > 0])
-    max_t = max([d["max_temperature"] for d in ld])
-    min_t = min([d["min_temperature"]
-                 for d in ld if d["min_temperature"] > 0])
-    h = statistics.mean([d["humidity"] for d in ld if d["humidity"] > 0])
-    min_h = min([d["min_humidity"]
-                 for d in ld if d["min_humidity"] > 0])
-    w = statistics.mean([d["wind_speed"] for d in ld])
-    g = max([d["max_gust"] for d in ld])
-    r = statistics.mean([d["precipitation"] for d in ld])
-    s = statistics.mean([d["sunshine"] for d in ld])
-    max_uv = max([d["max_uv"] for d in ld])
-    c = statistics.mean([d["cloud"] for d in ld])
-    return p, max_p, min_p, t, max_t, min_t, h, min_h, w, g, r, s, max_uv, c
+    try:
+        p = statistics.mean([d["pressure"] for d in ld if d["pressure"] > 0])
+        max_p = max([d["max_pressure"] for d in ld])
+        min_p = min([d["min_pressure"] for d in ld if d["min_pressure"] > 0])
+        t = statistics.mean([d["temperature"]
+                             for d in ld if d["temperature"] > 0])
+        max_t = max([d["max_temperature"] for d in ld])
+        min_t = min([d["min_temperature"]
+                     for d in ld if d["min_temperature"] > 0])
+        h = statistics.mean([d["humidity"] for d in ld if d["humidity"] > 0])
+        min_h = min([d["min_humidity"]
+                     for d in ld if d["min_humidity"] > 0])
+        w = statistics.mean([d["wind_speed"] for d in ld])
+        g = max([d["max_gust"] for d in ld])
+        r = statistics.mean([d["precipitation"] for d in ld])
+        s = statistics.mean([d["sunshine"] for d in ld])
+        max_uv = max([d["max_uv"] for d in ld])
+        c = statistics.mean([d["cloud"] for d in ld])
+        return p, max_p, min_p, t, max_t, min_t, h, min_h, w, g, r, s, max_uv, c
+
+    except Exception as e:
+        print(e)
