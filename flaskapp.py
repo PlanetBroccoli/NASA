1579c8f2c9a089f703cc4b450ee6a72b6bfc0962
mewturn <pathonice@hotmail.com>
2018-10-20 20:43:08 +0800

    v0.0.2 published

--- flaskapp.py
+++ flaskapp.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify
 import queries
 import calculate
 import datetime
+import json
 
 app = Flask(__name__)
 
@@ -9,13 +10,32 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def main():
     return render_template("index.html")
-    # return "Hello World"
+
+
+@app.route("/predictall", methods=["GET"])
+def predict_all():
+    all_coords = queries.getAllLocations()
+    output = []
+
+    id = 0
+    for coord in all_coords:
+        lat, lon = coord[0], coord[1]
+        city, district, prob = calculate.calculateCoords(lat, lon)
+        output.append(
+            {"lat": lat, "lon": lon, "id": id, "probability": prob})
+        id += 1
+        print(output)
+
+    return jsonify(content=output)
 
 
 @app.route("/predictcoords", methods=["GET"])
 def predict_coords():
     try:
         lat, lon = request.args.get("lat"), request.args.get("lon")
+        city, district, prob = calculate.calculateCoords(lat, lon)
+
+        return jsonify(time=datetime.datetime.now(), lat=lat, lon=lon, city=city, district=district, probability=prob)
 
     except Exception as e:
         print(e)
